8763f6c00e88894c24a8d4290badbb17c052174b
DenysHandziichuk <fleymidesign@gmail.com>
2025-12-22 21:54:10 -0500

    Add GenerateButton component for generating cheat sheets

---
+++ frontend/src/components/GenerateButton.jsx
@@ -0,0 +1,13 @@
+import '../styles/GenerateButton.css'
+
+export default function GenerateButton({ onClick, disabled }) {
+  return (
+    <button
+      className="generate-button"
+      onClick={onClick}
+      disabled={disabled}
+    >
+      Generate
+    </button>
+  );
+}
