73b26798ee750d33b699efbea9ebe97f43474a11
DenysHandziichuk <fleymidesign@gmail.com>
2025-12-22 21:54:21 -0500

    Add Skeleton component for loading placeholder UI

---
+++ frontend/src/components/Skeleton.jsx
@@ -0,0 +1,18 @@
+import '../styles/Skeleton.css'
+
+export default function Skeleton() {
+  return (
+    <div className="skeleton">
+      <div className="skeleton-title" />
+
+      {[1, 2, 3, 4].map((i) => (
+        <div key={i} className="skeleton-section">
+          <div className="skeleton-section-title" />
+          <div className="skeleton-line" />
+          <div className="skeleton-line" />
+          <div className="skeleton-line short" />
+        </div>
+      ))}
+    </div>
+  );
+}
